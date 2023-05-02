893c8a66b2ecdd38a56642d6fc19d42a325fc358
MSahrul <msahrul1608@gmail.com>
2023-05-02 15:12:26 +0700

    Patch framework

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -20,7 +20,7 @@ use Psr\Log\LoggerInterface;
  *
  * For security be sure to declare any new methods as protected or private.
  */
-class BaseController extends Controller
+abstract class BaseController extends Controller
 {
     /**
      * Instance of the main Request object.
@@ -46,6 +46,12 @@ class BaseController extends Controller
     protected $cabang;
     protected $modalName;
     protected $cabangAktif;
+
+    /**
+     * Be sure to declare properties for any property fetch you initialized.
+     * The creation of dynamic property is deprecated in PHP 8.2.
+     */
+    // protected $session;
 
     /**
      * Constructor.
